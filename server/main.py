f4697f3def7416fccdc648452e24a8477a65ed36
AzureAD\RafalPompecki <rafal@carlsolutions.com>
2018-02-13 14:37:35 +0100

    lines added before every funtion

--- server/main.py
+++ server/main.py
@@ -1,17 +1,21 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/")
 def main():
     return "Hey Main!"
 
+
 @app.route("/first")
 def hello():
     return "Hey First!"
- 
+
+
 @app.route("/second")
 def inny():
     return "Hey Second!"
+
 
 if __name__ == "__main__": 
     app.run(host="0.0.0.0", port=80)
